56c012247c5f11b40ede83132162779a1f92b18b
Rennan Nogueira <rmelo.nogueira123@gmail.com>
2022-09-12 13:10:42 -0300

    Read file

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,31 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import entities.Log;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List <Log> list = new ArrayList<>();
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter file full path: ");
+			String path = sc.nextLine();
+			try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+				String line = br.readLine();
+				while (line != null) {
+					String [] fields = line.split(" ");
+					System.out.println(line);
+					line = br.readLine();
+				}
+			} catch (IOException e) {
+				System.out.println("Error: " + e.getMessage());
+			}
+		}
 	}
-
 }
---
+++ src/entities/Log.java
@@ -0,0 +1,20 @@
+package entities;
+
+import java.util.Date;
+
+public class Log {
+	String name;
+	Date date;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}
