01bcbbeff1d037059fb7950f00e0d5676b1277c7
Rennan Nogueira <rmelo.nogueira123@gmail.com>
2022-09-12 15:12:03 -0300

    Completed

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,29 +3,36 @@ package application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 import entities.Log;
 
 public class Program {
 
 	public static void main(String[] args) {
-		List <Log> list = new ArrayList<>();
+		
 		try (Scanner sc = new Scanner(System.in)) {
 			System.out.println("Enter file full path: ");
 			String path = sc.nextLine();
 			try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+				Set <Log>set = new HashSet<>();
 				String line = br.readLine();
 				while (line != null) {
 					String [] fields = line.split(" ");
-					System.out.println(line);
-					line = br.readLine();
+					Date date = Date.from(Instant.parse(fields[1]));
+							set.add(new Log (fields[0],date));
+							line = br.readLine();
+					}
+				
+					
+					System.out.println("Total users: " + set.size());
 				}
 			} catch (IOException e) {
 				System.out.println("Error: " + e.getMessage());
 			}
 		}
 	}
-}
--- src/entities/Log.java
+++ src/entities/Log.java
@@ -1,6 +1,7 @@
 package entities;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Log {
 	String name;
@@ -14,7 +15,27 @@ public class Log {
 	public Date getDate() {
 		return date;
 	}
+	public Log(String name, Date date) {
+		super();
+		this.name = name;
+		this.date = date;
+	}
 	public void setDate(Date date) {
 		this.date = date;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Log other = (Log) obj;
+		return Objects.equals(name, other.name);
 	}
 }
